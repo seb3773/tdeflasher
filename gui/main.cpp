fff2a47703cd7a985b56bd7167c01c96c0c68d21
seb3773 <99963207+seb3773@users.noreply.github.com>
2026-08-30 00:16:16 +0200

    Fix TDE application initialization and plugin style compatibility

--- gui/main.cpp
+++ gui/main.cpp
@@ -1,9 +1,25 @@
 #include "mainwindow.h"
+
+#ifdef USE_TDE
+#include <tdeapplication.h>
+#include <tdeaboutdata.h>
+#include <tdecmdlineargs.h>
+#include <tdelocale.h>
+#else
 #include <ntqapplication.h>
 #include <tqt.h>
+#endif
 
 int main(int argc, char **argv) {
+#ifdef USE_TDE
+  TDEAboutData aboutData("tdeflasher", I18N_NOOP("tdeFlasher"), "1.0.0",
+                         I18N_NOOP("OS Image Flasher for Trinity Desktop"),
+                         TDEAboutData::License_GPL);
+  TDECmdLineArgs::init(argc, argv, &aboutData);
+  TDEApplication app;
+#else
   TQApplication app(argc, argv);
+#endif
 
   MainWindow win;
   app.setMainWidget(&win);
